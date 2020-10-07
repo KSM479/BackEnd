2226d2b99c9a8e7b731dad2a416f38abed104443
Kalpan Mehta <kalpan.mehta@gmail.com>
2020-10-07 16:22:12 -0400

    Changed to make WAR

--- src/main/java/com/Project/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/Project/springbootdemo/SpringbootdemoApplication.java
@@ -2,10 +2,15 @@ package com.Project.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
-
+public class SpringbootdemoApplication extends SpringBootServletInitializer{
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(SpringbootdemoApplication.class);
+	   }
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
